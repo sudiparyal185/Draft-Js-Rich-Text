56e1280b220b809f81882a53350ee44f1f74f460
Sudip Aryal <sudip.aryal@nextiva.com>
2022-10-20 20:16:00 -0500

    added basic editor

--- src/components/RichEditor/RichEditor.tsx
+++ src/components/RichEditor/RichEditor.tsx
@@ -1,11 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Editor, EditorState } from 'draft-js';
+import { Box } from '@mui/material';
 
 const RichEditor = () => {
+  const editorRef = useRef<any>(null);
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty(),
   );
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+  const focusEditor = () => {
+    editorRef?.current?.focus();
+  };
+  useEffect(() => {
+    focusEditor();
+  }, []);
+  return (
+    <Box
+      ref={editorRef}
+      sx={{
+        border: '1px solid black',
+        maxWidth: '800px',
+        minHeight: '500px',
+        ml: 5,
+      }}
+    >
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+        spellCheck={true}
+      />
+    </Box>
+  );
 };
 
 export { RichEditor };
